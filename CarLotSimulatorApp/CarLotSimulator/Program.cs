f83ebf24027e23179dd2862c62b62d2cee1af087
tyler davis <tyler.davis432@gmail.com>
2024-03-04 19:00:58 -0500

    complete carlot

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net.Http.Headers;
+using System.Xml;
 
 namespace CarLotSimulator
 {
@@ -17,6 +19,44 @@ namespace CarLotSimulator
             //Now that the Car class is created we can instanciate 3 new cars
             //Set the properties for each of the cars
             //Call each of the methods for each car
+            var Nissan = new car();
+            Nissan.Make = "Nissan";
+            Nissan.Year = 2019;
+            Nissan.Model = "altima";
+            Nissan.EngineNoise = "im fast BOY";
+            Nissan.HonkNoise = "beeeeep";
+            Nissan.IsDriveable = true;
+
+            var Toyota = new car();
+            Toyota.Make = "Toyota";
+            Toyota.Year = 2006;
+            Toyota.Model = "camery";
+            Toyota.HonkNoise = "Honk ";
+            Toyota.EngineNoise = "I'm slow";
+            Toyota.IsDriveable = false;
+
+
+
+
+
+            var Honda = new car()
+            {
+                Make = "Honda",
+                Year = 2022,
+                Model = "Honda ",
+                EngineNoise = "I'm iight vrmm ",
+                HonkNoise = "Mira Mira",
+                IsDriveable = true,
+            };
+
+            var tylerCar = new car(2012, "camery", "toyota", "vrrrmmm", "honk", true);
+
+
+            Console.WriteLine(Toyota.EngineNoise);
+            Console.WriteLine(Nissan.EngineNoise);
+            Console.WriteLine(Honda.EngineNoise);
+
+
 
             //*************BONUS*************//
 
---
+++ CarLotSimulatorApp/CarLotSimulator/car.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class car
+    {
+        public car(int year, 
+                   string make,
+                   string model, 
+                   string honkNoise, 
+                   string engineNoise, 
+                   bool isDrivable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            HonkNoise = honkNoise; 
+            EngineNoise = engineNoise;
+            IsDriveable = isDrivable;
+
+        }
+
+        public car() { }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNoise(string engineNoise)
+        {
+            Console.WriteLine(engineNoise);
+        }
+        public void MakeHonkNoise(string honkNoise)
+        {
+            Console.WriteLine(honkNoise);
+        }
+
+
+    }
+   
+    
+    
+}
